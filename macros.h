a4be02547cd835ed890938b578e0dcf305a4e549
samubernard <samubernard@gmail.com>
2017-11-28 15:55:09 +0100

    update macros.h

--- macros.h
+++ macros.h
@@ -1,4 +1,16 @@
-/* macros.h */
+/* file macros.h */
+#ifndef FILE_MACROS_SEEN
+#define FILE_MACROS_SEEN
+
+#define max(a,b) \
+       ({ __typeof__ (a) _a = (a); \
+           __typeof__ (b) _b = (b); \
+         _a > _b ? _a : _b; })
+
+#define min(a,b) \
+       ({ __typeof__ (a) _a = (a); \
+           __typeof__ (b) _b = (b); \
+         _a < _b ? _a : _b; })
 
 
 /* log file */
@@ -13,3 +25,5 @@
     ({ printf("--%s: ",__FUNCTION__); \
        printf( __VA_ARGS__); \
        printf(", in %s, line %d\n",__FILE__,__LINE__); })
+
+#endif /* !FILE_MACROS_SEEN */
