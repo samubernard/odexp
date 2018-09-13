0b6b2edeca0bed012f0244e61fbd99673d322ae9
samubernard <samubernard@gmail.com>
2018-09-13 12:42:41 +0200

    P declaration is optional

--- src/main.c
+++ src/main.c
@@ -76,7 +76,7 @@ struct gen_option GOPTS[NBROPTS] = {
     {"term","terminal", 's', 0.0, 0, "aqua", "gnuplot TERMinal (do not change in runtime)", "gnuplotSettings"},
     {"ld","loudness", 's', 0.0, 0, "loud", "LouDness mode silent | quiet | {loud} (silent not implemented)", "generalSettings"},
     {"fx","fix", 'i', 0.0, 4, "", "number of digits after decimal point {4}", "generalSettings"},
-    {"pr","progress", 'i', 0.0, 1, "", "print PRogress 0 | 1 | {2}", "generalSettings"} };
+    {"pr","progress", 'i', 0.0, 1, "", "print PRogress 0 | {1} | 2", "generalSettings"} };
 
 /* what kind of initial conditions to take */
 int *NUM_IC;
