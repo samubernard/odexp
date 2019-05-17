a3e498adb0785e3e410e5513ac7548ab7e79801d
samubernard <samubernard@gmail.com>
2019-05-17 17:33:07 +0200

    clean up codes and update man

--- src/lrexp.h
+++ src/lrexp.h
@@ -9,8 +9,8 @@
 typedef double (*coupling_function)(double, void *);
 
 int lrexpars(double *x, int N, double *meanx, double *range);
-int lrexporder(coupling_function f,int N, int *p, double range);
+int lrexprank(coupling_function f,int N, int *p, double range);
 int lrexp(coupling_function f, double *x, double *y, int N, int p, double meanx, double range);
 int lrexpw(coupling_function f, const double *U, const double *V, int r, double *x, double *y, int N, int p, double meanx, double range);
 int lrkern(coupling_function f, double *x, double *y, int N);
-int lrwkern(const double *U, const double *V, int r, coupling_function f, double *x, double *y, int N);
+int lrw(const double *U, const double *V, int r, coupling_function f, double *x, double *y, int N);
