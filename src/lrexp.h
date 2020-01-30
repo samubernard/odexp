7cac222a8be27aa3244404007e39a186141117db
samubernard <samubernard@gmail.com>
2020-01-30 16:20:14 +0100

    recovered lrexp

--- src/lrexp.h
+++ src/lrexp.h
@@ -6,7 +6,12 @@
  * in the most efficient way
  */
 
+#ifndef _LREXP_H_
+#define _LREXP_H_
+
+#ifndef _ODEXP_H_
 typedef double (*coupling_function)(double, void *);
+#endif
 
 int lrexpars(double *x, int N, double *meanx, double *range);
 int lrexprank(coupling_function f,int N, int *p, double range);
@@ -17,3 +22,5 @@ int lrkern(coupling_function f, double *x, double *y, int N);
 int lrwkern(const double *U, const double *V, int r, coupling_function f, double *x, double *y, int N);
 int lrwpkern(int iu, int iv, int r, coupling_function f, double *x, double *y, int N);
 int lrw(const double *U, const double *V, int r, coupling_function f, double *x, double *y, int N);
+
+#endif
